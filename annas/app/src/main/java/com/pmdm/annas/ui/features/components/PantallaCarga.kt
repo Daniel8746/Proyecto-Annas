88d4e87cbc014030c4530fcb463f746452db519f
Técnico <tecnico1@GAIA_AL.local>
2026-03-17 07:59:40 +0100

    Pantalla carga cambio a box de nuevo

--- annas/app/src/main/java/com/pmdm/annas/ui/features/components/PantallaCarga.kt
+++ annas/app/src/main/java/com/pmdm/annas/ui/features/components/PantallaCarga.kt
@@ -1,7 +1,6 @@
 package com.pmdm.annas.ui.features.components
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
@@ -20,9 +19,8 @@ import com.pmdm.annas.R
 
 @Composable
 fun PantallaCarga() {
-    Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
+    Box(
+        contentAlignment = Alignment.Center
     ) {
         CircularProgressIndicator(modifier = Modifier.size(48.dp))
         Spacer(modifier = Modifier.height(16.dp))
